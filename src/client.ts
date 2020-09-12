8c6dec47808f60eb5fd1e37464b26ab9a4d84ba2
Rohit singh <51336939+dprcsingh@users.noreply.github.com>
2020-09-12 15:25:18 +0530

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+const config = process.env.REACT_APP_GRAPHQL_URI;
+
 const client = new ApolloClient({
-  uri: 'https://e-comm-demo.herokuapp.com/graphql',
+  uri: config,
   cache: new InMemoryCache(),
 });
 export default client;
