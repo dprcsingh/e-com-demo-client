d77037781172a04291c73e44307190b7cb2d1336
Rohit Singh <dprcsingh@gmail.com>
2020-09-12 14:35:15 +0530

    resolved build error

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const config = process.env.REACT_APP_GRAPHQL_URI;
-
 const client = new ApolloClient({
-  uri: config,
+  uri: 'https://e-comm-demo.herokuapp.com/graphql',
   cache: new InMemoryCache(),
 });
 export default client;
